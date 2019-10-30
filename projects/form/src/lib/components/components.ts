378d8783312b93758869d1da91d14f0e22f0d605
ZimboPro <dejagerleslie@gmail.com>
2019-10-30 19:46:40 +0200

    improve controls

--- projects/form/src/lib/components/components.ts
+++ projects/form/src/lib/components/components.ts
@@ -1,11 +1,18 @@
 import { Subscription } from 'rxjs';
-import { OnDestroy } from '@angular/core';
+import { OnDestroy, OnInit } from '@angular/core';
 import { LibFormGroup, IControls } from '../reactive/reactive';
+import { ControlContainer } from '@angular/forms';
 
-export class ComponentBase<T extends IControls> implements OnDestroy{
+export class ComponentBase<T extends IControls> implements OnInit, OnDestroy{
     subscriptions: Subscription[] = [];
 
     form: LibFormGroup<T>;
+
+    constructor(private controlContainer: ControlContainer) {}
+
+    ngOnInit() {
+        this.form = <LibFormGroup<T>>this.controlContainer.control;
+    }
 
     ngOnDestroy() {
         this.form.clearValuesAndValidators();
--- projects/form/src/lib/reactive/control.ts
+++ projects/form/src/lib/reactive/control.ts
@@ -1,16 +1,39 @@
-import { FormControl, ValidatorFn } from '@angular/forms';
+import { FormControl, ValidatorFn, Validators } from '@angular/forms';
+
+import { ValidatorType } from '../validators/validator.type';
 
 export class LibFormControl extends FormControl {
-    
-    validatorFn: ValidatorFn[] = [];
-    appliedValidators: any;
+    private validators: {
+        [key: string]: ValidatorFn;
+    } = {};
 
-    addValidator() {
-        
+    private _convertValidatorsToArray() {
+        const validators = [];
+        for (const validator in this.validators) {
+            if (this.validators.hasOwnProperty(validator)) {
+                const element = this.validators[validator];
+                validators.push(element);
+            }
+        }
+        return validators;
     }
 
-    addValidators() {
+    private _updateValidators() {
+        const validators = this._convertValidatorsToArray();
+        this.setValidators(validators);
+        this.updateValueAndValidity();
+    }
 
+    addValidator(type: ValidatorType | string, validator: ValidatorFn) {
+        this.validators[type] = validator;
+        this._updateValidators();
+    }
+    
+    addValidators(validators: {type: ValidatorType | string, validator: ValidatorFn}[]) {
+        for (const validator of validators) {
+            this.validators[validator.type] = validator.validator;
+        }
+        this._updateValidators();
     }
 
     setReadonly() {
@@ -21,15 +44,20 @@ export class LibFormControl extends FormControl {
 
     }
 
-    removeValidator() {
-
+    removeValidator(type: ValidatorType | string) {
+        delete this.validators[type];
+        this._updateValidators();
     }
 
-    removeValidators() {
-        
+    removeValidators(types: ValidatorType[] | string[]) {
+        for (const type of types) {
+            delete this.validators[type];
+        }
+        this._updateValidators();
     }
 
     clearValidators() {
+        this.validators = {};
         super.clearValidators();
     }
 
