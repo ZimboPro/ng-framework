dcebdd0ec24fe2c9952f1c3e0471b9ccbbf8e4d4
Leslie <dejagerleslie@gmail.com>
2019-11-08 10:24:57 +0200

    added interface lib

---
+++ projects/lib/interfaces/src/lib/interfaces.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-interfaces',
+  template: `
+    <p>
+      interfaces works!
+    </p>
+  `,
+  styles: []
+})
+export class InterfacesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/lib/interfaces/src/lib/interfaces.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { InterfacesComponent } from './interfaces.component';
+
+
+
+@NgModule({
+  declarations: [InterfacesComponent],
+  imports: [
+  ],
+  exports: [InterfacesComponent]
+})
+export class InterfacesModule { }
